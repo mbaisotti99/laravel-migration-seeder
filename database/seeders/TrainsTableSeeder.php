c67652785e5fb346e7c6d25ed058e251eb84a1d8
mbaisotti99 <matteo.baisotti@gmail.com>
2025-03-28 01:00:14 +0100

    constrain

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -18,24 +18,32 @@ class TrainsTableSeeder extends Seeder
         for ($i = 0; $i < 100; $i++) {
             $oraPart = $faker->dateTimeBetween('-3 months', Carbon::now()->endOfYear());
             $oraArr = Carbon::instance($oraPart)->addHours(rand(1, 10))->addMinutes(rand(0, 59));
+            
             $newTrain = new Train();
+
             $newTrain->azienda = $faker->company();
             $newTrain->stazPartenza = $faker->city();
             $newTrain->stazArrivo = $faker->city();
             $newTrain->oraPartenza = $oraPart;
             $newTrain->oraArrivo = $oraArr;
+
             do {
                 $cod = strtoupper($faker->bothify("??####"));
             } while (Train::where('codice', $cod)->exists());
             $newTrain->codice = $cod;
+            
             $newTrain->nCarrozze = $faker->randomDigit();
+            
             $inOr = $faker->boolean();
             $newTrain->inOrario = $inOr;
+            
             $newTrain->cancellato = $faker->boolean(30);
             $newTrain->nome = $faker->name();
+            
             if (!$inOr){
                 $newTrain->ritardo = rand(1,100);
             }
+            
             $newTrain->save();
         }
     }
--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [MainController::class, "index"]);
 Route::get("/lost", [MainController::class,"lost"]);
-Route::get("/corsa/{id}", [MainController::class,"get"]);
+Route::get("/corsa/{id}", [MainController::class,"get"])->where(['id' => '[0-9]+']);
